890a625b3e13c84f4892374a64eaead191260137
HariPrasadGuntukolu <guntukoluhariprasad@gmail.com>
2025-05-18 17:15:03 +0530

    Updated Loading.jsx

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -4,7 +4,12 @@ import "./loading.css";
 const Loading = () => {
   return (
     <div className="loading-page">
-      <div className="loader"></div>
+      <div className="glow-loader">
+        <span className="rotating-circle"></span>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
