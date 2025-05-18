4ef936bb042ac518d25f4c2aeef5a7a049982e31
HariPrasadGuntukolu <guntukoluhariprasad@gmail.com>
2025-05-18 21:01:42 +0530

    Updated Header component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi";
 
 const Header = ({ isAuth }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <header>
-      <div className="logo">UpSkill</div>
-      <div className="link">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/courses"}>Courses</Link>
-        <Link to={"/about"}>About</Link>
-        {isAuth ? (
-          <Link to={"/account"}>Account</Link>
-        ) : (
-          <Link to={"/login"}>Login</Link>
-        )}
+      <div className="logo" onClick={closeMenu}>
+        UpSkill
       </div>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <FiX /> : <FiMenu />}
+      </div>
+
+      <nav className={`link ${menuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={closeMenu}>
+          Home
+        </Link>
+        <Link to="/courses" onClick={closeMenu}>
+          Courses
+        </Link>
+        <Link to="/about" onClick={closeMenu}>
+          About
+        </Link>
+        {isAuth ? (
+          <Link to="/account" onClick={closeMenu}>
+            Account
+          </Link>
+        ) : (
+          <Link to="/login" onClick={closeMenu}>
+            Login
+          </Link>
+        )}
+      </nav>
     </header>
   );
 };
