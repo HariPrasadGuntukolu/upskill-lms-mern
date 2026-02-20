32d53522b5f9ed57ac4bf4c47dd6713b65448c59
HariPrasadGuntukolu <guntukoluhariprasad@gmail.com>
2026-02-20 17:29:37 +0530

    Update server URL to local development address

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-<<<<<<< HEAD
 export const server = " http://localhost:5000";
-=======
-export const server = "https://upskill-mnqd.onrender.com";
->>>>>>> a84fe58cee9ab35ae75a81cf44526805485f2dfc
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
