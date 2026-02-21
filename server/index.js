0961f2bcecc3204b02d28c5e583d4699c5e07e18
HariPrasadGuntukolu <guntukoluhariprasad@gmail.com>
2026-02-21 13:56:07 +0530

    login issue

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import Razorpay from "razorpay";
 import cors from "cors";
 import axios from "axios";
 
-const url = `https://upskill-lms-mern.onrender.com`;
+const url = `http://localhost:5000/`;
 const interval = 30000;
 
 function reloadWebsite() {
